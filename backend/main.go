4eecd9aff2093c9c6843b4dc40e6bbdbef5c500f
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2025-05-11 18:46:08 +0200

    feat(*): Added DB connection

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("Hello world")
+
+	// Load env file
+	err := godotenv.Load("./backend/config.env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.Background()
+
+	// Get DB connection
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		os.Exit(1)
+	}
+	defer conn.Close(ctx)
+
 }
