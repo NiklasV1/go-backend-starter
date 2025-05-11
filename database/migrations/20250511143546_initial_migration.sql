f432acfffc8fe766c0ce6272f86cfdd2a1ba55e8
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2025-05-11 16:49:48 +0200

    feat(*): Added intial DB migration

---
+++ database/migrations/20250511143546_initial_migration.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TABLE customers (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    address TEXT NOT NULL,
+    username TEXT NOT NULL UNIQUE,
+    password BYTEA NOT NULL
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE customers;
+-- +goose StatementEnd
